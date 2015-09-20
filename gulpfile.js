7dc4601157551ea72efec393568876e52cca9a35
Ice <ice@IcedeMBP.lan>
2015-09-20 22:33:08 +0800

    init gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,140 +1,66 @@
 var
+
 	gulp = require('gulp'),
-	sourcemaps = require('gulp-sourcemaps'),
-	source = require('vinyl-source-stream'),
-	react = require('gulp-react'),
-	del = require('del'),
-	browserify = require('browserify'),
-	watchify = require('watchify'),
-	reactify = require('reactify'),
 	$ = require('gulp-load-plugins')(),
-
 	browserSync = require('browser-sync'),
-	reload = browserSync.reload,
-
-	less = require('gulp-less'),
-
 	jshint = require('gulp-jshint'),
 	stylish = require('jshint-stylish'),
+	del = require('del'),
 
 	JSXPath = 'app/scripts',
 	JSXFile = JSXPath + '/app.js',
-
 
 	STYLEPath = 'app/style',
 	STYLEFile = STYLEPath + '/**/*.less',
 
 	HTMLFile = 'app/**/*.html',
 
+	IMAGEFile = 'app/images/**/*',
+
 	destFileName = 'app.js',
 
 	DISTPath = 'dist'
 
-var bundler = watchify(browserify({
-	entries: [JSXFile],
-	debug: true,
-	insertGlobals: true,
-	cache: {},
-	packageCache: {},
-	transform: ['reactify'],
-	fullPaths: true
-}))
 
-bundler.on('update', rebundle)
-bundler.on('log', $.util.log)
-
-function rebundle() {
-	return bundler.bundle()
-		.on('error', $.util.log.bind($.util, 'Browserify Error'))
-		.pipe(source(destFileName))
-		.pipe(gulp.dest(DISTPath))
-		.on('end', function() {
-			reload()
-		})
-}
-
-// Scripts
-gulp.task('scripts', rebundle)
-
-gulp.task('buildScripts', function() {
-    return browserify(JSXFile)
-        .bundle()
-        .pipe(source(DISTPath))
-        .pipe(gulp.dest(DISTPath + '/scripts'))
-})
-
-//HTML
-gulp.task('html', function() {
-    return gulp.src(HTMLFile)
-        .pipe($.useref())
-        .pipe(gulp.dest(DISTPath))
-        .pipe($.size())
-})
-
-
-gulp.task('bundle', ['styles', 'scripts'], function() {
-    return gulp.src(HTMLFile)
-        .pipe($.useref.assets())
-        .pipe($.useref())
-        .pipe(gulp.dest(DISTPath));
-});
-
-gulp.task('buildBundle', ['styles', 'buildScripts'], function() {
-    return gulp.src(HTMLFile)
-        .pipe($.useref.assets())
-        .pipe($.useref())
-        .pipe(gulp.dest(DISTPath));
-});
-
-
-gulp.task('del', function() {
-
-	del([DISTPath])
-})
-
-gulp.task('lint', function() {
-
-	return gulp.src(JSXFile)
-		.pipe(jshint({
-			linter: require('jshint-jsx').JSXHINT
-		}))
-		.pipe(jshint.reporter(stylish));
-})
-
+//style files
 gulp.task('styles', function() {
 
 	return gulp.src(STYLEFile)
-		.pipe(less())
+		.pipe($.less())
 		.pipe(gulp.dest(DISTPath + '/style'))
+		.pipe(browserSync.stream())
 })
 
-gulp.task('react', ['del'], function() {
+//html files
+gulp.task('html', function() {
+	return gulp.src(HTMLFile)
+		.pipe($.useref())
+		.pipe(gulp.dest(DISTPath))
+		.pipe($.size())
+})
 
-	return gulp.src(JSXFile)
-		.pipe(sourcemaps.init())
-		.pipe(react())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(DISTPath + '/scripts'))
+gulp.task('del', function() {
+
+	$.del([DISTPath])
 })
 
 
-
-gulp.task('watch', ['html', 'bundle'], function() {
+gulp.task('watch', ['html', 'styles'], function() {
 
     browserSync({
         notify: true,
-        logPrefix: 'BS',
-        server: ['dist', 'app']
+        logPrefix: 'react',
+        server: ['dist']
     });
 
-    // Watch .json files
-    gulp.watch(JSXFile, ['scripts']);
+    gulp.watch([HTMLFile, IMAGEFile])
+    	.on('change', browserSync.reload)
 
     // Watch .html files
-    gulp.watch(HTMLFile, ['html']);
+    gulp.watch(HTMLFile, ['html'])
 
-    gulp.watch([STYLEFile], ['styles', reload]);
+    gulp.watch([STYLEFile], ['styles']);
 
     // Watch image files
-    gulp.watch('app/images/**/*', reload);
+    gulp.watch('app/images/**/*');
 });
